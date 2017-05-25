6453525b1171c6b841b130df77acc61cd7ce8939
alber <alber@ALBERTB>
2017-05-25 17:53:31 +0800

    moree stuff

--- SORT/SORT.cpp
+++ SORT/SORT.cpp
@@ -3,33 +3,43 @@
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
+
+void thisisthesorterfunction(int&array1, int&array2)
+{
+	int temp;
+	temp = array1;
+	array1 = array2;
+	array2 = temp;
+}
+
 int main()
 {
-	int a[10] = { 0 }, b;
+	int a[10] = { 3,2,4,1,5,8,6,7,9,10}, b=0;
+	int asorted[10];
 	srand(time(NULL));
 
 	for (int r = 0; r < 10; r++)
 	{
-		a[r] = 1 + rand() % 10;
 		cout << a[r]<<"  ";
+		asorted[r] = a[r];
 	}
-	cout << "\n\n\n";
-	for (int c = 0; c < 11; c++)
+	cout << endl;
+
+	for (int r = 0; r < 10; r++)
 	{
-		for (int r = 0; r < 10; r++)
+		for (int j = 0; j < 9 - r; j++)
 		{
-			if (a[r] > a[r + 1])
-			{
-				b = a[r];
-				a[r + 1] = a[r];
-				a[r] = b;
-			}
+			if (asorted[j] > asorted[j + 1])
+				thisisthesorterfunction(asorted[j], asorted[j + 1]);
 		}
 	}
-	for (int r = 0; r > 10; r++)
+
+	cout << "Sorted: ";
+	for (int r = 0; r < 10; r++)
 	{
-		cout << a[r]<<"  ";
+		cout << asorted[r] << " ";
 	}
+
 	_getch();
 	return 0;
 }
